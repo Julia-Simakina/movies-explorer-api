28ae0001e1455bc543aded73fda1a1608e076c03
Julia Simakina <simakina.js@yandex.ru>
2023-09-18 19:46:15 +0300

    feat: user and movie schema

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+});
+
+const app = express();
+
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+app.listen(PORT, () => {
+  console.log('слушаю 3000');
+});
---
+++ models/movie.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const { urlRegex } = require('../constants/urlRegex');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  duration: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator(url) {
+        return urlRegex.test(url);
+      },
+      message: 'Введён некорректный URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator(url) {
+        return urlRegex.test(url);
+      },
+      message: 'Введён некорректный URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator(url) {
+        return urlRegex.test(url);
+      },
+      message: 'Введён некорректный URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  movieId: {
+    type: Number,
+    ref: 'user',
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
